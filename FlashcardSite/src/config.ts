c10f67f2925624c3e3975a344edca8327cfaf7ce
Johnny Sun <sunj@Mac.home>
2026-04-23 20:58:12 -0500

    Change db connection for aws

--- FlashcardSite/src/config.ts
+++ FlashcardSite/src/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://localhost:5036";
+// export const API_BASE_URL = "http://localhost:5036";
+export const API_BASE_URL = "https://fl-5a6a5660a64d428381a84756dca43d46.ecs.us-east-1.on.aws";
